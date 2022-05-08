6f85fa8c84e4c6d3f6db66b3147ee810d67b9bb1
Anish Bisht <anish-bisht@student.allenisd.org>
2022-05-07 23:27:28 -0500

    added background music to menu

--- Sources/ScenesShell/MenuBackground.swift
+++ Sources/ScenesShell/MenuBackground.swift
@@ -12,8 +12,16 @@ class MenuBackground : RenderableEntity {
     var clearRectangle = Rectangle(rect: Rect())
     let whiteFill = FillStyle(color: Color(.white))
 
+    let music: Audio
+    var isBackgroundPlaying = false
+
 
     init() {
+        guard let musicURL = URL(string: "https://codermerlin.com/users/anish-bisht/menu.mp3") else {
+            fatalError("could not get menu music url")
+        }
+        music = Audio(sourceURL: musicURL, shouldLoop: true)
+        
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Background")
     }
@@ -21,11 +29,15 @@ class MenuBackground : RenderableEntity {
     override func setup(canvasSize: Size, canvas: Canvas) {
         clearRect = Rect(topLeft: Point.zero, size: canvasSize)
         clearRectangle = Rectangle(rect: clearRect, fillMode: .fill)
+        canvas.setup(music)
     }
 
     override func render(canvas: Canvas) {
+        if  music.isReady {
+            canvas.render(music)
+            isBackgroundPlaying = true
+        }
         canvas.render(whiteFill, clearRectangle)
     }
-
     
 }
